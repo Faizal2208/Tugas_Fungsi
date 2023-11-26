10dbadd08d68e41cdedb7f40a23a18eedea22b3d
Mochamad Fa'izal Fanani <148428114+Faizal2208@users.noreply.github.com>
2023-11-26 20:16:30 +0700

    Create Tugas fungsi 2.py

---
+++ Tugas fungsi 2.py
@@ -0,0 +1,15 @@
+#nama : mochamad faizal fanani
+#no absen : 14
+#kelas : XI TKJ 1
+
+def hitung_pangkat(bilangan, eksponen):
+    hasil = bilangan ** eksponen
+    return hasil
+
+# Mengambil input bilangan dan eksponen dari pengguna
+bilangan = float(input("Masukkan bilangan: "))
+eksponen = int(input("Masukkan eksponen: "))
+
+# Memanggil fungsi dan mencetak hasilnya
+hasil_pangkat = hitung_pangkat(bilangan, eksponen)
+print(f"{bilangan}^{eksponen} = {hasil_pangkat}")
